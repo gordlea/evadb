97a7389dbeac92a7ab99667bfcec648cd3a795b1
Gord Lea <jgordonlea@gmail.com>
2023-11-08 10:00:08 -0800

    fix: don't limit version of pydantic

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ minimal_requirements = [
     "aenum>=2.2.0",
     "diskcache>=5.4.0",
     "retry>=0.9.2",
-    "pydantic<2",  # ray-project/ray#37019.
+    "pydantic",
     "psutil",
     "thefuzz",
 ]
